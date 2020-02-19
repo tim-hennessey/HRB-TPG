0808e875633ff359d85336ae8e4ad4ce852111e2
tim.hennessey <Tim.Hennessey@deutschinc.com>
2020-02-19 11:40:15 -0800

    Creative Update

---
+++ app/300x600/scripts/Animation.js
@@ -0,0 +1,69 @@
+var app = app || {};
+
+
+app.Animation = (function () {
+
+	var banner = document.getElementById('banner');
+	var t = TweenMax;
+	var tl1 = new TimelineMax({repeat:2});
+	var tl2 = new TimelineMax();
+	var img1 = document.getElementById('img1');
+	var img2 = document.getElementById('img2');
+	var img3 = document.getElementById('img3');
+	var img4 = document.getElementById('img4');
+	var img5 = document.getElementById('img5');
+
+	var cta = document.getElementById('cta');
+	var cta_txt = document.getElementsByClassName('cta_txt');
+	var buttonExit = document.getElementById('button-exit');
+
+	// --------------------------------------------------------------------------------------
+	// set default properties
+	function initialize() {
+		// DO NOT EDIT: reveals banner once loaded
+		t.set(banner, {opacity:1});
+
+		buttonExit.addEventListener('mouseover', function () {
+            t.to(cta, .25, {backgroundColor: "#3bc0f8"});
+            t.to(cta_txt, .25, {fill: "#262626" });
+        });
+
+        buttonExit.addEventListener('mouseout', function () {
+            t.to(cta, .25, {backgroundColor: "#f5cc02"});
+            t.to(cta_txt, .25, {fill: "#000" });
+        });
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Starts the animation
+	function start() {
+
+		tl1.set(img1, {display:'none'}, "+=.45")
+		.set(img2, {display:'none'}, "+=.45")
+		.set(img3, {display:'none'}, "+=.45")
+		.set(img4, {display:'none'}, "+=.45")
+		.set(img5, {display:'none'}, "+=.45");
+
+		tl2.to(txt1, .25, {opacity:1})
+		.to(txt1, .25, {opacity:0}, "+=3")
+
+		.to(txt3, .25, {opacity:1})
+
+		.to(txt2, .25, {opacity:1}, "+=2");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
